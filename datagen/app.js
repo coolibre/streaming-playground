17a19f266eab79554a971a6f83adb16bcd0023b2
coolibre <58222491+coolibre@users.noreply.github.com>
2022-08-21 19:59:12 +0200

    use 2 streams

--- datagen/app.js
+++ datagen/app.js
@@ -19,8 +19,8 @@ async function sleep(ms) {
 }
 
 async function run() {
-  async function write(val) {
-    const message = new Buffer(`temperature,sensor=sensor1 value=${val}\n`);
+  async function write(topic, val) {
+    const message = new Buffer(`${topic},sensor=sensor1 ${topic}=${val}\n`);
     client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
       if (err) console.error("ERROR: ", err);
       console.log("UDP client message sent to " + HOST + ":" + PORT);
@@ -33,7 +33,8 @@ async function run() {
     }
     upwards ? (value += 0.1) : (value -= 0.1);
     count -= 1;
-    await write(value);
+    await write("temperature", value);
+    await write("speed", value * 3);
     await sleep(100);
   }
 }
